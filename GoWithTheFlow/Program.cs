d4caca54a054d93b30cbc3034355f2e32f8a99af
Jael <jaele.jones@gmail.com>
2016-06-14 15:42:18 -0400

    First Commit

---
+++ GoWithTheFlow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoWithTheFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are your Eyes Watching?");
+            Console.WriteLine("Yes or No");
+            Console.ReadLine();
+            string answer = Console.ReadLine();
+
+            if (answer == yes);
+            {
+                // child typed typed "Yes"
+                Console.WriteLine("You are a good listener!");
+            }
+            else
+            {
+                // child did not type "yes"
+                Console.WriteLine("Keep your eyes on me, please!");
+            }
+        }
+    }
+}
