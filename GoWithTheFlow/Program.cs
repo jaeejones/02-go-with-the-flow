84e8166cd21b4c857c99beb5cbeecfac1f35598c
Jael <jaele.jones@gmail.com>
2016-06-17 12:04:42 -0400

    Added loops in the flow chart

--- GoWithTheFlow/Program.cs
+++ GoWithTheFlow/Program.cs
@@ -11,39 +11,53 @@ namespace GoWithTheFlow
         static void Main(string[] args)
         {
             while (true)
-            { 
+            {
 
 
-            Console.WriteLine("Are your eyes on the teacher?");
-            Console.WriteLine("Is your mouth closed?");
-            Console.WriteLine("Are your ears listening?");
-            Console.WriteLine("Are your hands and feet still? ");
-            Console.WriteLine("Type yes or no");  //Changed to type yes or no
-   
-            string answer = Console.ReadLine();
-
-                if (answer.ToLower() == "yes")
+                Console.WriteLine("Are your eyes on the teacher?");
+                string eyes = Console.ReadLine();
+                if (eyes == "yes")
                 {
-                    // child typed typed "Yes" this code is executed.
-                    Console.WriteLine("You are a very good listener!");
-                    Console.WriteLine("Press any key to continue");
-                    Console.ReadLine();
+                    Console.WriteLine("Is your mouth closed?");
+                    Console.WriteLine("Are your ears listening?");
+                    Console.WriteLine("Are your hands and feet still?");
+                    Console.WriteLine("Type yes or no");
+
+                    
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() == "yes")
+                    {
+                        // child typed typed "Yes" this code is executed.
+                        Console.WriteLine("You are a very good listener!");
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadLine();
 
 
+                    }
+                    else
+                    {
+                        // child did not type "yes" this code is executed. 
+                        Console.WriteLine("Try again, you can do!");
+
+                        /*Console.ReadLine();*/
+
+                        // added Console.ReadKey and commented out Console.ReadLine
+
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
+                    }
                 }
+
                 else
                 {
-                    // child did not type "yes" this code is executed. 
-                    Console.WriteLine("Try again, you can do!");
-
-                    /*Console.ReadLine();*/
-
-                    // added Console.ReadKey and commented out Console.ReadLine
-
-                    Console.WriteLine("Press any key to continue");
+                    Console.WriteLine("You can do it, Try Again! Press enter to try again!");
                     Console.ReadKey();
                 }
+
+
+               
+
+            }
             }
         }
     }
-}
